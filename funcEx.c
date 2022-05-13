5a586e824660f2fa8b5ec9a55ebdcb9ff827f4dc
Heung <80873447+sinheungheung@users.noreply.github.com>
2022-05-13 12:08:35 +0900

    함수를 사용해서 수가 짝수인지 홀수인지 알아보기

--- funcEx.c
+++ funcEx.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
+display(int su, char ch)
+{
+	for (int i = 1; i <= su; i++)
+		printf("%c", ch);
+}
+ // 2의 배수인지 아닌지를 판별하는 함수
+int isEven(int n)
+{
+	return(n % 2 == 0 ? 1 : 0);
+}
 
 int main(void)
 {
 	display(20,'#');
-	
+	if (isEven(10) == 1)
+		printf("\n짝수입니다.");
+	else
+		printf("\n홀수입니다.");
 	return 0;
 }
 
- display(int su, char ch)
-{
-	for (int i = 1; i <= su; i++)
-	printf("%c",ch);
-		
-}
+ 
