feaadfb5b78fcc6d99d62a491b2a1cca5c43ea0e
Heung <80873447+sinheungheung@users.noreply.github.com>
2022-05-20 11:46:50 +0900

    Call by reference

---
+++ callbyref.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int* add(int x, int y) {
+	int sum;
+	sum = x + y;
+	return &sum;
+}
+int main(void) {
+	int x = 5, y = 7;
+	int* pz;
+	pz = add(x, y);
+	printf("%d\n", *pz); //12
+	pz = add(x, y);
+	printf("%d\n", *pz); //³Î°ª
+	
+	return 0;
+}
